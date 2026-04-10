a07660e97d4754ede55e476cdc55d953660651ca
swapna162 <kswapna388@gmail.com>
2026-04-10 21:25:16 +0530

    kubernatees file

---
+++ src/main/java/com/cisco/KubernatesdeploymentdApplication.java
@@ -0,0 +1,13 @@
+package com.cisco;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KubernatesdeploymentdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KubernatesdeploymentdApplication.class, args);
+	}
+
+}
